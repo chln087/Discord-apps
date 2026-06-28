09c234af48345b8a1a4cd439c423d1798ab00a30
ch1nn0877 <chrx485@gmail.com>
2026-06-28 15:19:25 +0800

    Refactor setup command in setup.py
    Refactor setup command to use followup response and store guild_id in a variable.

--- cogs/setup.py
+++ cogs/setup.py
@@ -2,7 +2,6 @@ import sqlite3
 import discord
 from discord import app_commands
 from discord.ext import commands
-from typing import Optional
 
 class Setup(commands.Cog):
     def __init__(self, bot):
@@ -17,6 +16,9 @@ class Setup(commands.Cog):
         verify_role: discord.Role,
     ):
         await interaction.response.defer()
+
+        guild_id = interaction.guild_id
+        
 ###############獲取id#############
         conn = sqlite3.connect("data/settings.db")
         cursor = conn.cursor()
@@ -26,7 +28,7 @@ class Setup(commands.Cog):
             (guild_id, welcome_channel, verify_role)
             VALUES (?, ?, ?)
             """, (
-                interaction.guild_id,
+                guild_id,
                 welcome_channel.id,
                 verify_role.id
             ))
@@ -35,7 +37,7 @@ class Setup(commands.Cog):
         conn.close()
 
 ###############回應###############
-        await interaction.response.send_message(f"設定成功！", ephemeral=True)
+        await interaction.followup.send("設定成功！", ephemeral=True)
 
 async def setup(bot):
     await bot.add_cog(Setup(bot))
